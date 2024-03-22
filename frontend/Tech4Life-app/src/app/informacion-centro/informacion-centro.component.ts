4ad64c8673103bc0f97f0995fc7fe3a015f140f7
RaulgbUMA <100631828+RaulgbUMA@users.noreply.github.com>
2024-03-22 19:39:39 +0100

    Creación componente informacion-centro
    Esto es una primera versión del componente referente a la información del centro que gestiona el gerente en cuestión.
    He modificado ciertos archivos del componente principal para que la aplicación pudiera reconocer mi componente (Realizando imports)

---
+++ frontend/Tech4Life-app/src/app/informacion-centro/informacion-centro.component.ts
@@ -0,0 +1,19 @@
+/* 
+  informacion-centro.component.ts
+  Esto es el typescript del componente que ha realizado Raúl
+*/
+import { Component } from '@angular/core';
+
+// Aquí hay que importar todos los componentes que vaya a usar este componente.
+// Por ahora ninguno.
+
+@Component({
+  selector: 'app-informacion-centro',
+  standalone: true,
+  imports: [],
+  templateUrl: './informacion-centro.component.html',
+  styleUrl: './informacion-centro.component.css'
+})
+export class InformacionCentroComponent {
+
+}
