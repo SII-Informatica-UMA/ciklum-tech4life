018f0fc7bb4467c1c93110b07533b2e2211db530
Anamariagonz7 <anamariagonzr7@gmail.com>
2024-03-26 10:53:47 +0100

    componentes1

--- frontend/Tech4Life-app/src/app/app.component.ts
+++ frontend/Tech4Life-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component'
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [BarraNavegacionComponent],
+  templateUrl: './barra-navegacion/barra-navegacion.component.html',
+  styleUrl: './barra-navegacion/barra-navegacion.component.css'
+})
+
 export class AppComponent {
   title = 'Tech4Life-app';
 }
+
