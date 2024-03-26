8927ff7466239e1a7243b622eafb8e38024bbcf0
Anamariagonz7 <anamariagonzr7@gmail.com>
2024-03-26 12:23:30 +0100

    carrusel

--- frontend/Tech4Life-app/src/app/app.component.ts
+++ frontend/Tech4Life-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component'
+import { CarruselComponent } from './carrusel/carrusel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,BarraNavegacionComponent],
+  imports: [RouterOutlet,BarraNavegacionComponent,CarruselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
