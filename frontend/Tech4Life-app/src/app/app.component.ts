08aa6bd50eec3f8cf31cabefdb16632e3fd457c5
Ana Martín <anamartin@uma.es>
2024-04-01 22:21:41 +0200

    V4-V5 terminado

--- frontend/Tech4Life-app/src/app/app.component.ts
+++ frontend/Tech4Life-app/src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListaCentrosComponent } from "./lista-centros/lista-centros.component";
 import { ListaGerentesComponent } from './lista-gerentes/lista-gerentes.component';
+import { DetallesCentroComponent } from './detalles-centro/detalles-centro.component';
+import { FormularioCentroComponent } from './formulario-centro/formulario-centro.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routes';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
 @Component({
     selector: 'app-root',
     standalone: true,
