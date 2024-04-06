04d10d3092072ca9b09b4df23a5d12403f30aa99
Anamariagonz7 <anamariagonzr7@gmail.com>
2024-04-06 14:04:57 +0200

    cambiosGerente

--- frontend/Tech4Life-app/src/app/barra-navegacion-gerente/barra-navegacion-gerente.component.ts
+++ frontend/Tech4Life-app/src/app/barra-navegacion-gerente/barra-navegacion-gerente.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { InformacionComponent } from '../informacion/informacion.component';
+import { ContactosService } from '../usuario.service';
+import { Usuario } from '../usuario';
 
 @Component({
   selector: 'app-barra-navegacion-gerente',
@@ -13,5 +15,9 @@ import { InformacionComponent } from '../informacion/informacion.component';
 export class BarraNavegacionGerenteComponent {
   logo = './assets/logoT4L.png'
   isDropdownOpen = false;
-  
+  usuario: Usuario | undefined;
+  constructor(private contactosService: ContactosService) { }
+  ngOnInit(): void {
+  this.usuario = this.contactosService.getLogin();  
+}
 }
