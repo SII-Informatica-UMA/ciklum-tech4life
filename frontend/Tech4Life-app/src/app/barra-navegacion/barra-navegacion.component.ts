0d7fde8e9854526f0341f1dacedb90737815327e
Anamariagonz7 <anamariagonzr7@gmail.com>
2024-04-06 11:10:55 +0200

    probando

--- frontend/Tech4Life-app/src/app/barra-navegacion/barra-navegacion.component.ts
+++ frontend/Tech4Life-app/src/app/barra-navegacion/barra-navegacion.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-barra-navegacion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './barra-navegacion.component.html',
   styleUrl: './barra-navegacion.component.css'
 })
 export class BarraNavegacionComponent {
 logo = './assets/logoT4L.png'
+isDropdownOpen = false;
+
+open(): void{
+  this.isDropdownOpen=true;
 }
+
+}
+
+
