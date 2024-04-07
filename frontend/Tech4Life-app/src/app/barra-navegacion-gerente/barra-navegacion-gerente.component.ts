81809344c9b6f5b3caf413398400668519d3fafd
Anamariagonz7 <anamariagonzr7@gmail.com>
2024-04-07 16:17:32 +0200

    enlaces p2

--- frontend/Tech4Life-app/src/app/barra-navegacion-gerente/barra-navegacion-gerente.component.ts
+++ frontend/Tech4Life-app/src/app/barra-navegacion-gerente/barra-navegacion-gerente.component.ts
@@ -4,15 +4,31 @@ import { FormsModule } from '@angular/forms';
 import { InformacionComponent } from '../informacion/informacion.component';
 import { ContactosService } from '../usuario.service';
 import { Usuario } from '../usuario';
+import { CarruselComponent } from '../carrusel/carrusel.component';
+import { InformacionCentroComponent } from '../informacion-centro/informacion-centro.component';
 
 @Component({
   selector: 'app-barra-navegacion-gerente',
   standalone: true,
-  imports: [CommonModule, FormsModule, InformacionComponent],
+  imports: [CommonModule, FormsModule, InformacionComponent, CarruselComponent, InformacionCentroComponent],
   templateUrl: './barra-navegacion-gerente.component.html',
   styleUrl: './barra-navegacion-gerente.component.css'
 })
 export class BarraNavegacionGerenteComponent {
+  isMenu = true;
+  isInfoCentro = false;
+  cerrarVentanas(){
+  this.isMenu=false;
+  this.isInfoCentro=false;  
+  }
+abrirMenu() {
+  this.cerrarVentanas();
+  this.isMenu=true;
+}
+abrirInfoCentro(){
+  this.cerrarVentanas();
+  this.isInfoCentro=true;
+}
   logo = './assets/logoT4L.png'
   isDropdownOpen = false;
   usuario: Usuario | undefined;
