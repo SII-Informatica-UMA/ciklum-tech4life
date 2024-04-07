6c6cae06c95167eec6ab40a12ed06801c84d404a
Anamariagonz7 <anamariagonzr7@gmail.com>
2024-04-07 13:59:17 +0200

    enlazando

--- frontend/Tech4Life-app/src/app/barra-navegacion/barra-navegacion.component.ts
+++ frontend/Tech4Life-app/src/app/barra-navegacion/barra-navegacion.component.ts
@@ -4,19 +4,44 @@ import { FormsModule } from '@angular/forms';
 import { InformacionComponent } from '../informacion/informacion.component';
 import { ContactosService } from '../usuario.service';
 import { Usuario } from '../usuario';
+import { ListaGerentesComponent } from '../lista-gerentes/lista-gerentes.component';
+import { Router } from '@angular/router';
+import { ListaCentrosComponent } from '../lista-centros/lista-centros.component';
+import { CarruselComponent } from '../carrusel/carrusel.component';
 
 @Component({
   selector: 'app-barra-navegacion',
   standalone: true,
-  imports: [CommonModule, FormsModule, InformacionComponent],
+  imports: [CommonModule, FormsModule, InformacionComponent, ListaGerentesComponent, ListaCentrosComponent, CarruselComponent],
   templateUrl: './barra-navegacion.component.html',
   styleUrl: './barra-navegacion.component.css'
 })
 export class BarraNavegacionComponent {
+  constructor(private contactosService: ContactosService) {}
+  isGestionGerentes = false;
+  isGestionCentros = false;
+  isMenu = true;
+  abrirMenu(){
+    this.cerrarVentanas();
+    this.isMenu=true;
+  }
+  cerrarVentanas(){
+    this.isGestionGerentes = false;
+    this.isGestionCentros = false;
+    this.isMenu=false;
+  }
+  abrirGerentes(){
+    this.cerrarVentanas();
+    this.isGestionGerentes=true;
+  }
+  abrirCentros(){
+    this.cerrarVentanas();
+    this.isGestionCentros=true;
+  }
+
 logo = './assets/logoT4L.png'
 isDropdownOpen = false;
 usuario: Usuario | undefined;
-constructor(private contactosService: ContactosService) { }
 ngOnInit(): void {
   this.usuario = this.contactosService.getLogin();
 }
