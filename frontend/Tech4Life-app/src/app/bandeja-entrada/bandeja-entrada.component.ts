4b26c23821b4e9912c4fc44d797a126e722ecec5
bonvieana <bonvieana@gmail.com>
2024-04-08 12:22:30 +0200

    Componente bandeja entrada creado

---
+++ frontend/Tech4Life-app/src/app/bandeja-entrada/bandeja-entrada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bandeja-entrada',
+  standalone: true,
+  imports: [],
+  templateUrl: './bandeja-entrada.component.html',
+  styleUrl: './bandeja-entrada.component.css'
+})
+export class BandejaEntradaComponent {
+
+}
