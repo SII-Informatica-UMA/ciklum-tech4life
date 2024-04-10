258183b5f053952dd6e48c34e6a1831a7fd045eb
Anamariagonz7 <anamariagonzr7@gmail.com>
2024-04-10 18:48:08 +0200

    cambios

--- frontend/Tech4Life-app/src/app/app.routes.ts
+++ frontend/Tech4Life-app/src/app/app.routes.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ForgottenPasswordComponent } from './forgotten-password/forgotten-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ListadoUsuarioComponent } from './listado-usuario/listado-usuario.component';
+import { PrincipalComponent } from './principal/principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'forgotten-password',
+    component: ForgottenPasswordComponent
+  },
+  {
+    path: 'reset-password',
+    component: ResetPasswordComponent
+  },
+  {
+    path: 'usuarios',
+    component: ListadoUsuarioComponent
+  },
+  {
+    path: '',
+    component: PrincipalComponent
+  }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
