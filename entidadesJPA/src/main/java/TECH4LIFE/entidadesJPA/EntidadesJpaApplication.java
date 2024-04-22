feb9aed0279fd950c0b097ac889889206fa7b489
RaulgbUMA <100631828+RaulgbUMA@users.noreply.github.com>
2024-04-22 18:24:19 +0200

    Creación carpeta entidadesJPA
    Empezamos la T2

---
+++ entidadesJPA/src/main/java/TECH4LIFE/entidadesJPA/EntidadesJpaApplication.java
@@ -0,0 +1,13 @@
+package TECH4LIFE.entidadesJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntidadesJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntidadesJpaApplication.class, args);
+	}
+
+}
---
+++ entidadesJPA/src/test/java/TECH4LIFE/entidadesJPA/EntidadesJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package TECH4LIFE.entidadesJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntidadesJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
