2085c828cf0ea6fc845822a559b98bc1232f3084
RaulgbUMA <100631828+RaulgbUMA@users.noreply.github.com>
2024-04-24 18:15:07 +0200

    Ultimas modificaciones

--- entidadesJPA/src/main/java/TECH4LIFE/entidadesJPA/entities/Mensaje.java
+++ entidadesJPA/src/main/java/TECH4LIFE/entidadesJPA/entities/Mensaje.java
@@ -4,12 +4,39 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Setter
 @Getter
 @Entity
 public class Mensaje {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mensaje mensaje = (Mensaje) o;
+        return Objects.equals(idMensaje, mensaje.idMensaje) && Objects.equals(asunto, mensaje.asunto) && Objects.equals(destinatarios, mensaje.destinatarios) && Objects.equals(copia, mensaje.copia) && Objects.equals(copiaOculta, mensaje.copiaOculta) && Objects.equals(remitente, mensaje.remitente) && Objects.equals(contenido, mensaje.contenido);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idMensaje, asunto, destinatarios, copia, copiaOculta, remitente, contenido);
+    }
+
+    @Override
+    public String toString() {
+        return "Mensaje{" +
+                "idMensaje=" + idMensaje +
+                ", asunto='" + asunto + '\'' +
+                ", destinatarios=" + destinatarios +
+                ", copia=" + copia +
+                ", copiaOculta=" + copiaOculta +
+                ", remitente=" + remitente +
+                ", contenido='" + contenido + '\'' +
+                '}';
+    }
 
     @Embeddable // This class can be separate
     public static class Destinatario {
