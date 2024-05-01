258967803489a6a39c4d85ad6674c4ed5fe9cb7b
Ana Martín <anamartin@uma.es>
2024-05-01 12:32:40 +0200

    Controlador Gerente y LogicaGerente

--- entidadesJPA/src/main/java/TECH4LIFE/entidadesJPA/controladores/ControladorGerente.java
+++ entidadesJPA/src/main/java/TECH4LIFE/entidadesJPA/controladores/ControladorGerente.java
@@ -1,8 +1,114 @@
 package TECH4LIFE.entidadesJPA.controladores;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import TECH4LIFE.entidadesJPA.dtos.GerenteDTO;
+import TECH4LIFE.entidadesJPA.dtos.GerenteNuevoDTO;
+import TECH4LIFE.entidadesJPA.entities.Gerente;
+import TECH4LIFE.entidadesJPA.excepciones.GerenteExistente;
+import TECH4LIFE.entidadesJPA.excepciones.GerenteNoExistente;
+import TECH4LIFE.entidadesJPA.servicios.LogicaGerente;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ControladorGerente {
+    
+    private LogicaGerente servicio;
+
+    public ControladorGerente(LogicaGerente servicioGerente){
+        servicio = servicioGerente;
+    }
+
+    //GET Lista Gerentes
+    @GetMapping
+    public ResponseEntity<List<GerenteDTO>> listaGerentes(){
+
+        return ResponseEntity.ok(servicio.getGerentes().stream().map(Mapper::toGerenteDTO).toList());
+    }
+
+    //GET Gerente {idGerente}
+    @GetMapping("id")
+    public ResponseEntity<?> obtenerGerentePorId(@PathVariable(name="id") Integer id){
+        try{
+            Optional<Gerente> gerenteOptional = servicio.getGerente(id);
+            if(gerenteOptional.isPresent()){ 
+            
+                GerenteDTO gerenteDTO = Mapper.toGerenteDTO(gerenteOptional.get());
+                //Todo bien 200
+                return ResponseEntity.ok(gerenteDTO);
+            } else {
+                //No encontrado 404
+                return ResponseEntity.notFound().build();
+            }
+        }catch(UsuarioNoAutorizado e){
+            //No autorizado 403
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+           
+    }
+
+    
+    //PUT Gerente {idGerente}
+    @PutMapping("{id}")
+    public ResponseEntity modificarGerente(@PathVariable(name="id") Integer id, @RequestBody GerenteNuevoDTO gerente){
+
+        try{
+            servicio.modificarGerente(id, gerente);
+           // [200] El gerente se ha actualizado
+            return ResponseEntity.ok().build();
+        }catch (GerenteNoExistente e){
+            // [404] Gerente no existente
+            return ResponseEntity.notFound().build();
+        }catch (GerenteExistente e){
+            // [403] Acceso no autorizado
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+    //DELETE Gerente {idGerente}
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> eliminarGerentePorId(@PathVariable(name="id") Integer id){
+        try{
+            servicio.eliminarGerente(id);
+             // [200] El gerente se ha borrado con éxito
+            return ResponseEntity.ok().build();
+        }catch (GerenteNoExistente e){
+             // [404] Gerente no existente
+             return ResponseEntity.notFound().build();
+        }catch(UsuarioNoAutorizado e){
+             // [403] Acceso no autorizado
+             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        
+        }
+    }
+    //POST Lista Gerentes
+     
+    @PostMapping
+    public ResponseEntity<GerenteDTO> addGerente(@RequestBody GerenteNuevoDTO gerente, UriComponentsBuilder builder ){
+
+        try{
+            Gerente nuevoGerente = servicio.addGerente(gerente);
+            URI uri= builder
+                .path("/gerente")
+                .path(String.format("/%d", nuevoGerente.getId()))
+                .build()
+                .toUri();
+            return ResponseEntity.created(uri).body(Mapper.toGerenteDTO(nuevoGerente));  
+        }catch (GerenteNoExistente e){
+            // [404] Not Found
+            return ResponseEntity.notFound().build();
+       }catch(GerenteExistente e){
+            // [403] Acceso no autorizado
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+       
+       }
+
+
+    }
+
 
 }
--- entidadesJPA/src/main/java/TECH4LIFE/entidadesJPA/servicios/LogicaGerente.java
+++ entidadesJPA/src/main/java/TECH4LIFE/entidadesJPA/servicios/LogicaGerente.java
@@ -1,11 +1,77 @@
 package TECH4LIFE.entidadesJPA.servicios;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import TECH4LIFE.entidadesJPA.dtos.GerenteNuevoDTO;
+import TECH4LIFE.entidadesJPA.entities.Gerente;
+import TECH4LIFE.entidadesJPA.excepciones.GerenteExistente;
+import TECH4LIFE.entidadesJPA.excepciones.GerenteNoExistente;
+import TECH4LIFE.entidadesJPA.repositories.GerenteRepository;
 
 @Service
 @Transactional
 public class LogicaGerente {
+    private GerenteRepository repo;
+    //Devuelve lista de gerentes
+    public List<Gerente> getGerentes() {
+        return repo.findAll();
+    }
+    //Devuelve un gerente por id
+    public Optional<Gerente> getGerente(Integer id) {
+        return repo.findById(id);
+    }
+
+    //Modificar Gerente
+    public void modificarGerente(Integer id, GerenteNuevoDTO gerente) {
+        //HAY QUE MANEJAR LA EXCEPCION DE USUARIONOAUTORIZADO
+        Optional<Gerente> gerenteExistente = repo.findById(id);
+
+        if( gerenteExistente.isEmpty()){
+            throw new GerenteNoExistente();
+        }
+       
+        Gerente gerenteAModificar = gerenteExistente.get();
+        gerenteAModificar.setEmpresa(gerente.getEmpresa());
+        gerenteAModificar.setIdUsuario(gerente.getIdUsuario());
+        if(repo.findById(gerenteAModificar.getIdUsuario()).isPresent()){
+            throw new GerenteExistente();
+        }
+       
+        repo.save(gerenteAModificar);
+    }
+
+    //Eliminar un gerente
+    public void eliminarGerente(Integer id) {
+        //HAY QUE MANEJAR LA EXCEPCION DE USUARIONOAUTORIZADO
+        Optional<Gerente> gerenteOptional = repo.findById(id);
+        if(gerenteOptional.isPresent()){
+            repo.deleteById(id);
+        }else{
+            throw new GerenteNoExistente();
+        }
+    }
+
+    //Añadir un Gerente
+    public Gerente addGerente(GerenteNuevoDTO gerente) {
+       
+        if(repo.findById(gerente.getIdUsuario()).isPresent()){
+            throw new GerenteExistente();
+        }
+
+        Gerente nuevoGerente = new Gerente();
+        nuevoGerente.setEmpresa(gerente.getEmpresa());
+        nuevoGerente.setIdUsuario(gerente.getIdUsuario());
+        return repo.save(nuevoGerente);
+    
+    }
+
+   
+
+
 
 
 }
