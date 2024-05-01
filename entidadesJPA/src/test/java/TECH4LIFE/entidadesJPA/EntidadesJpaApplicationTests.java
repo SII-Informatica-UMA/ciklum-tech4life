505f684abbd48c59eae3914178838ed8fa5e05b8
RaulgbUMA <100631828+RaulgbUMA@users.noreply.github.com>
2024-05-01 12:42:50 +0200

    Preparación inicial del archivo de los tests

--- entidadesJPA/src/test/java/TECH4LIFE/entidadesJPA/EntidadesJpaApplicationTests.java
+++ entidadesJPA/src/test/java/TECH4LIFE/entidadesJPA/EntidadesJpaApplicationTests.java
@@ -1,10 +1,33 @@
 package TECH4LIFE.entidadesJPA;
 
+import TECH4LIFE.entidadesJPA.repositories.CentroRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
-class EntidadesJpaApplicationTests {
+import java.net.URI;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisplayName("En el servicio de de gestión de centros y gerentes")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class EntidadesJpaApplicationTests {
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Value(value="${local.server.port}")
+	private int port;
+
+	@Autowired
+	private CentroRepository centroRepository;
+
+
 
 	@Test
 	void contextLoads() {
