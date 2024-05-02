8b82cf930ce05bb4613aa3c266e705a6ccba9630
RaulgbUMA <100631828+RaulgbUMA@users.noreply.github.com>
2024-05-02 21:49:51 +0200

    Primer contacto con las pruebas de JUNIT
    Me parecía interesante y para adelantar trabajo he empezado a familiarizarme con las pruebas

--- entidadesJPA/src/test/java/TECH4LIFE/entidadesJPA/EntidadesJpaApplicationTests.java
+++ entidadesJPA/src/test/java/TECH4LIFE/entidadesJPA/EntidadesJpaApplicationTests.java
@@ -1,13 +1,26 @@
 package TECH4LIFE.entidadesJPA;
 
+import TECH4LIFE.entidadesJPA.controladores.Mapper;
+import TECH4LIFE.entidadesJPA.dtos.CentroDTO;
+import TECH4LIFE.entidadesJPA.dtos.CentroNuevoDTO;
 import TECH4LIFE.entidadesJPA.repositories.CentroRepository;
+import TECH4LIFE.entidadesJPA.repositories.GerenteRepository;
+import TECH4LIFE.entidadesJPA.repositories.MensajeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
 
 import java.net.URI;
 import java.util.List;
@@ -26,11 +39,89 @@ public class EntidadesJpaApplicationTests {
 
 	@Autowired
 	private CentroRepository centroRepository;
+	@Autowired
+	private GerenteRepository gerenteRepository;
+	@Autowired
+	private MensajeRepository mensajeRepository;
 
-
-
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	public void initializeDatabase() {
+		centroRepository.deleteAll();
+		gerenteRepository.deleteAll();
+		mensajeRepository.deleteAll();
 	}
 
+	// Estoy trasteando
+
+	private URI uri(String scheme, String host, int port, String ...paths) {
+		UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+		UriBuilder ub = ubf.builder()
+				.scheme(scheme)
+				.host(host).port(port);
+		for (String path: paths) {
+			ub = ub.path(path);
+		}
+		return ub.build();
+	}
+
+	private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+		URI uri = uri(scheme, host,port, path);
+		var peticion = RequestEntity.get(uri)
+				.accept(MediaType.APPLICATION_JSON)
+				.build();
+		return peticion;
+	}
+
+
+	@Nested
+	@DisplayName("Cuando no hay centros")
+	public class ListaCentrosVacia{
+		@Test
+		@DisplayName("Devuelve la lista de centros vacía")
+		public void devuelveLista() {
+
+			var peticion = get("http", "localhost",port, "/centro"); // Revisar el path
+
+			var respuesta = restTemplate.exchange(peticion,
+					new ParameterizedTypeReference<List<CentroDTO>>() {});
+
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+			assertThat(respuesta.getBody()).isEmpty();
+		}
+	}
+
+	@Nested
+	@DisplayName("Cuando hay centros")
+	public class ListaCentrosConDatos {
+
+		private CentroNuevoDTO centro1 = CentroNuevoDTO.builder()
+				.nombre("BasicFit")
+				.direccion("Calle la calle bonita, 56")
+				.build();
+
+		private CentroNuevoDTO centro2 = CentroNuevoDTO.builder()
+				.nombre("ProGYM")
+				.direccion("Calle avestruz, 44")
+				.build();
+
+
+		@BeforeEach
+		public void introduceDatos() {
+			centroRepository.save(Mapper.toCentro(centro1));
+			centroRepository.save(Mapper.toCentro(centro2));
+		}
+
+		@Test
+		@DisplayName("Devuelve la lista de centros correctamente")
+		public void devuelveLista() {
+			var peticion = get("http", "localhost", port, "/centro"); // Revisar el path
+
+			var respuesta = restTemplate.exchange(peticion,
+					new ParameterizedTypeReference<List<CentroDTO>>() {
+					});
+
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+			assertThat(respuesta.getBody()).hasSize(2);
+		}
+	}
 }
