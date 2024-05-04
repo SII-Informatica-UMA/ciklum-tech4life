a43bc35e4c342934c0024725a2922a209c3519d2
bonvieana <bonvieana@gmail.com>
2024-05-05 01:28:24 +0200

    Empezando con los tests

--- entidadesJPA/src/test/java/TECH4LIFE/entidadesJPA/EntidadesJpaApplicationTests.java
+++ entidadesJPA/src/test/java/TECH4LIFE/entidadesJPA/EntidadesJpaApplicationTests.java
@@ -2,7 +2,10 @@ package TECH4LIFE.entidadesJPA;
 import TECH4LIFE.entidadesJPA.controladores.Mapper;
 import TECH4LIFE.entidadesJPA.dtos.CentroDTO;
 import TECH4LIFE.entidadesJPA.dtos.CentroNuevoDTO;
+import TECH4LIFE.entidadesJPA.dtos.MensajeDTO;
+import TECH4LIFE.entidadesJPA.dtos.MensajeNuevoDTO;
 import TECH4LIFE.entidadesJPA.entities.Centro;
+import TECH4LIFE.entidadesJPA.entities.Mensaje;
 import TECH4LIFE.entidadesJPA.repositories.CentroRepository;
 import TECH4LIFE.entidadesJPA.repositories.GerenteRepository;
 import TECH4LIFE.entidadesJPA.repositories.MensajeRepository;
@@ -58,7 +61,7 @@ public class EntidadesJpaApplicationTests {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
-	@Value(value="${local.server.port}")
+	@Value(value = "${local.server.port}")
 	private int port;
 
 	/*
@@ -94,19 +97,19 @@ public class EntidadesJpaApplicationTests {
 	---------------------------------------------
 	*/
 
-	private URI uri(String scheme, String host, int port, String ...paths) {
+	private URI uri(String scheme, String host, int port, String... paths) {
 		UriBuilderFactory ubf = new DefaultUriBuilderFactory();
 		UriBuilder ub = ubf.builder()
 				.scheme(scheme)
 				.host(host).port(port);
-		for (String path: paths) {
+		for (String path : paths) {
 			ub = ub.path(path);
 		}
 		return ub.build();
 	}
 
 	private RequestEntity<Void> get(String scheme, String host, int port, String path) {
-		URI uri = uri(scheme, host,port, path);
+		URI uri = uri(scheme, host, port, path);
 		var peticion = RequestEntity.get(uri)
 				.accept(MediaType.APPLICATION_JSON)
 				.build();
@@ -114,14 +117,14 @@ public class EntidadesJpaApplicationTests {
 	}
 
 	private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
-		URI uri = uri(scheme, host,port, path);
+		URI uri = uri(scheme, host, port, path);
 		var peticion = RequestEntity.delete(uri)
 				.build();
 		return peticion;
 	}
 
 	private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
-		URI uri = uri(scheme, host,port, path);
+		URI uri = uri(scheme, host, port, path);
 		var peticion = RequestEntity.post(uri)
 				.contentType(MediaType.APPLICATION_JSON)
 				.body(object);
@@ -129,7 +132,7 @@ public class EntidadesJpaApplicationTests {
 	}
 
 	private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
-		URI uri = uri(scheme, host,port, path);
+		URI uri = uri(scheme, host, port, path);
 		var peticion = RequestEntity.put(uri)
 				.contentType(MediaType.APPLICATION_JSON)
 				.body(object);
@@ -226,7 +229,7 @@ public class EntidadesJpaApplicationTests {
 					.nombre("KKFit")
 					.direccion("Calle la calle KK, 56")
 					.build();
-			var peticion = put("http", "localhost",port, "/centro", centro);
+			var peticion = put("http", "localhost", port, "/centro", centro);
 
 			var respuesta = restTemplate.exchange(peticion, Void.class);
 
@@ -236,7 +239,7 @@ public class EntidadesJpaApplicationTests {
 		@Test
 		@DisplayName("devuelve error cuando se elimina un centro concreto")
 		public void devuelveErrorAlEliminarCentro() {
-			var peticion = delete("http", "localhost",port, "/centro");
+			var peticion = delete("http", "localhost", port, "/centro");
 
 			var respuesta = restTemplate.exchange(peticion, Void.class);
 
@@ -377,22 +380,22 @@ public class EntidadesJpaApplicationTests {
 			public void eliminaCorrectamenteCentro() {
 				//List<Centro> centrosAntes = centroRepository.findAll();
 				//centrosAntes.forEach(c->System.out.println(c));
-				var peticion = delete("http", "localhost",port, "/centro/1");
+				var peticion = delete("http", "localhost", port, "/centro/1");
 
-				var respuesta = restTemplate.exchange(peticion,Void.class);
+				var respuesta = restTemplate.exchange(peticion, Void.class);
 
 				assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
 				List<Centro> centros = centroRepository.findAll();
 				assertThat(centros).hasSize(1);
-				assertThat(centros).allMatch(c->c.getIdCentro()!=1);
+				assertThat(centros).allMatch(c -> c.getIdCentro() != 1);
 			}
 
 			@Test
 			@DisplayName("da error cuando no existe")
 			public void errorCuandoNoExisteCentro() {
-				var peticion = delete("http", "localhost",port, "/centro/28");
+				var peticion = delete("http", "localhost", port, "/centro/28");
 
-				var respuesta = restTemplate.exchange(peticion,Void.class);
+				var respuesta = restTemplate.exchange(peticion, Void.class);
 
 				assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
 				assertThat(respuesta.hasBody()).isEqualTo(false);
@@ -413,13 +416,269 @@ public class EntidadesJpaApplicationTests {
 	/*
 	---------------------------------------------
 	 Pruebas de ControladorMensaje y LogicaMensaje
-	 Realizado por:
+	 Realizado por: Ana María Calvente
 	---------------------------------------------
 	*/
 
-	// TO DO
+	private void compruebaCamposMensaje(MensajeDTO expected, MensajeDTO actual) {
+		assertThat(actual.getAsunto()).isEqualTo(expected.getAsunto());
+		assertThat(actual.getContenido()).isEqualTo(expected.getContenido());
+	}
+
+	private void compruebaCamposMensaje(MensajeNuevoDTO expected, MensajeDTO actual) {
+		assertThat(actual.getAsunto()).isEqualTo(expected.getAsunto());
+		assertThat(actual.getContenido()).isEqualTo(expected.getContenido());
+	}
+
+	private void compruebaCamposMensaje(MensajeNuevoDTO expected, Mensaje actual) {
+		assertThat(actual.getAsunto()).isEqualTo(expected.getAsunto());
+		assertThat(actual.getContenido()).isEqualTo(expected.getContenido());
+	}
+
+	@Nested
+	@DisplayName("Cuando no hay centros")
+	public class ListaCentrosVacia {
+
+		@Test
+		@DisplayName("Devuelve la lista de centros vacía")
+		public void devuelveListaCentro() {
+
+			var peticion = get("http", "localhost", port, "/centro"); // Revisar el path
+
+			var respuesta = restTemplate.exchange(peticion,
+					new ParameterizedTypeReference<List<CentroDTO>>() {
+					});
+
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+			assertThat(respuesta.getBody()).isEmpty();
+		}
+
+		@Nested
+		@DisplayName("Intenta insertar un centro")
+		public class InsertaCentro {
+			@Test
+			@DisplayName("y se guarda con éxito")
+			public void sinID() {
+				var centro = CentroNuevoDTO.builder()
+						.nombre("egeFIT")
+						.direccion("Calle merluza, 56")
+						.build();
+				var peticion = post("http", "localhost", port, "/centro", centro);
+
+				var respuesta = restTemplate.exchange(peticion, Void.class);
+
+				compruebaRespuestaCentro(centro, respuesta);
+			}
+
+			private void compruebaRespuestaCentro(CentroNuevoDTO centro, ResponseEntity<Void> respuesta) {
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(201);
+				assertThat(respuesta.getHeaders().get("Location").get(0))
+						.startsWith("http://localhost:" + port + "/centro");
+
+				List<Centro> centros = centroRepository.findAll();
+				assertThat(centros).hasSize(1);
+				assertThat(respuesta.getHeaders().get("Location").get(0))
+						.endsWith("/" + centros.get(0).getIdCentro());
+				compruebaCamposCentro(centro, centros.get(0));
+			}
+		}
+
+		@Test
+		@DisplayName("Devuelve error cuando se pide un centro concreto")
+		public void devuelveErrorAlConsultarCentro() {
+			var peticion = get("http", "localhost", port, "/centro");
+
+			var respuesta = restTemplate.exchange(peticion,
+					new ParameterizedTypeReference<List<CentroDTO>>() {
+					});
+
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+			assertThat(respuesta.hasBody()).isEqualTo(false);
+		}
+
+		@Test
+		@DisplayName("devuelve error cuando se modifica un centro concreto")
+		public void devuelveErrorAlModificarCentro() {
+			var centro = CentroNuevoDTO.builder()
+					.nombre("KKFit")
+					.direccion("Calle la calle KK, 56")
+					.build();
+			var peticion = put("http", "localhost", port, "/centro", centro);
+
+			var respuesta = restTemplate.exchange(peticion, Void.class);
+
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+		}
+
+		@Test
+		@DisplayName("devuelve error cuando se elimina un centro concreto")
+		public void devuelveErrorAlEliminarCentro() {
+			var peticion = delete("http", "localhost", port, "/centro");
+
+			var respuesta = restTemplate.exchange(peticion, Void.class);
+
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+		}
+	}
+
+	@Nested
+	@DisplayName("Cuando hay centros")
+	public class ListaCentrosConDatos {
+
+		private CentroNuevoDTO centro1 = CentroNuevoDTO.builder()
+				.nombre("BasicFit")
+				.direccion("Calle la calle bonita, 56")
+				.build();
+
+		private CentroNuevoDTO centro2 = CentroNuevoDTO.builder()
+				.nombre("ProGYM")
+				.direccion("Calle avestruz, 44")
+				.build();
+
+
+		@BeforeEach
+		public void introduceDatosCentro() {
+			centroRepository.save(Mapper.toCentro(centro1));
+			centroRepository.save(Mapper.toCentro(centro2));
+		}
+
+		@Test
+		@DisplayName("Devuelve la lista de centros correctamente")
+		public void devuelveListaCentro() {
+			var peticion = get("http", "localhost", port, "/centro"); // Revisar el path
+
+			var respuesta = restTemplate.exchange(peticion,
+					new ParameterizedTypeReference<List<CentroDTO>>() {
+					});
+
+			assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+			assertThat(respuesta.getBody()).hasSize(2);
+		}
+
+		@Nested
+		@DisplayName("Intenta insertar un centro")
+		public class InsertaCentros {
+
+			// TO DO
+
+			@Test
+			@DisplayName("")
+			public void prueba1() {
+				// TO DO
+			}
+
+			@Test
+			@DisplayName("")
+			public void prueba2() {
+				// TO DO
+			}
+
+			private void compruebaRespuesta(CentroNuevoDTO centro, ResponseEntity<Void> respuesta) {
+				// TO DO
+			}
+		}
+
+		@Nested
+		@DisplayName("Al consultar un centro concreto")
+		public class ObtenerCentros {
+
+			@Test
+			@DisplayName("lo devuelve cuando existe")
+			public void devuelveCentro() {
+				var peticion = get("http", "localhost", port, "/centro/1");
+
+				var respuesta = restTemplate.exchange(peticion, CentroDTO.class);
+
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+				assertThat(respuesta.hasBody()).isTrue();
+				assertThat(respuesta.getBody()).isNotNull();
+			}
+
+			@Test
+			@DisplayName("da error cuando no existe")
+			public void errorCuandoCentroNoExiste() {
+				var peticion = get("http", "localhost", port, "/centro/28");
+
+				var respuesta = restTemplate.exchange(peticion,
+						new ParameterizedTypeReference<List<CentroDTO>>() {
+						});
+
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+				assertThat(respuesta.hasBody()).isEqualTo(false);
+			}
+		}
+
+		@Nested
+		@DisplayName("Al modificar un centro")
+		public class ModificarCentros {
+
+			@Test
+			@DisplayName("Lo modifica correctamente cuando existe")
+			@DirtiesContext
+			public void modificaCorrectamenteCentro() {
+				var centro = CentroNuevoDTO.builder()
+						.nombre("PepeFit")
+						.direccion("Calle la gaviota, 56")
+						.build();
+
+				var peticion = put("http", "localhost", port, "/centro/1", centro);
+
+				var respuesta = restTemplate.exchange(peticion, CentroDTO.class);
+
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+				Centro centroBD = centroRepository.findById(1).get();
+				compruebaCamposCentro(centro, centroBD);
+			}
+
+			@Test
+			@DisplayName("Da error cuando no existe")
+			public void errorCuandoNoExiste() {
+				var centro = CentroNuevoDTO.builder()
+						.nombre("PepeitoFit")
+						.direccion("Calle la gata, 56")
+						.build();
+				var peticion = put("http", "localhost", port, "/centro/28", centro);
+
+				var respuesta = restTemplate.exchange(peticion, Void.class);
+
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+				assertThat(respuesta.hasBody()).isEqualTo(false);
+			}
+		}
+
+		@Nested
+		@DisplayName("Al eliminar un centro")
+		public class EliminarCentros {
+			@Test
+			@DisplayName("Lo elimina cuando existe")
+			public void eliminaCorrectamenteCentro() {
+				//List<Centro> centrosAntes = centroRepository.findAll();
+				//centrosAntes.forEach(c->System.out.println(c));
+				var peticion = delete("http", "localhost", port, "/centro/1");
+
+				var respuesta = restTemplate.exchange(peticion, Void.class);
+
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+				List<Centro> centros = centroRepository.findAll();
+				assertThat(centros).hasSize(1);
+				assertThat(centros).allMatch(c -> c.getIdCentro() != 1);
+			}
+
+			@Test
+			@DisplayName("da error cuando no existe")
+			public void errorCuandoNoExisteCentro() {
+				var peticion = delete("http", "localhost", port, "/centro/28");
+
+				var respuesta = restTemplate.exchange(peticion, Void.class);
+
+				assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+				assertThat(respuesta.hasBody()).isEqualTo(false);
+			}
+		}
+	}
 
 }
+
 
 
 
