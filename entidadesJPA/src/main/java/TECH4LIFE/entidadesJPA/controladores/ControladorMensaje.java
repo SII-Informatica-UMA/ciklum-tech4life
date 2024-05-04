f9eef0cbdd0ef0d4888d65f85e95fba76f985a99
bonvieana <bonvieana@gmail.com>
2024-05-04 10:43:43 +0200

    Fin de mensaje 3

--- entidadesJPA/src/main/java/TECH4LIFE/entidadesJPA/controladores/ControladorMensaje.java
+++ entidadesJPA/src/main/java/TECH4LIFE/entidadesJPA/controladores/ControladorMensaje.java
@@ -33,8 +33,7 @@ public class ControladorMensaje {
  */
     //OBTENGO TODOS LOS MENSAJES DE UN CENTRO DADO EL CENTRO
     @GetMapping("/{centro}")    //capturo el objeto centro.
-    // DUDA: En PathVariable hay que poner (name="idMensaje")??
-    public ResponseEntity<List<MensajeDTO>> listaDeMensajes(@PathVariable Centro centro){   //guardo en la variable centro el objeto Centro
+    public ResponseEntity<List<MensajeDTO>> listaDeMensajes(@PathVariable (name="centro") Centro centro){   //guardo en la variable centro el objeto Centro
         try{
             //CODE 200: Devuelve la lista de mensajes de cierto centro
             List<MensajeDTO> listaMensajes = servicio.getMensajesByCentro(centro);
@@ -47,9 +46,6 @@ public class ControladorMensaje {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
-    /*
-    DEL MÉTODO ANTERIOR FALTA RESOLVER LA DUDA EN GETMENSAJESBYCENTRO
-     */
 //------------------------------------------------------------------------------------------
 
 
@@ -72,18 +68,13 @@ public class ControladorMensaje {
 /*
     POST
  */
-    //Cuando el usuario vaya a enviar un mensaje y se cree un mensaje, se detectará que el remitente es el centro, de modo
-    //que no hay que contemplar esto aquí.
-    /*
-        DUDA: creeis que lo que digo anteriormente es correcto o debería incluir una variable Centro?
-     */
-    @PostMapping
-    public ResponseEntity<MensajeDTO> crearMensaje(@RequestBody MensajeNuevoDTO mensajeNuevoDTO, UriComponentsBuilder builder) {
+    @PostMapping("/{centro}")
+    public ResponseEntity<MensajeDTO> crearMensaje(@PathVariable (name="centro") Centro centro, @RequestBody MensajeNuevoDTO mensajeNuevoDTO, UriComponentsBuilder builder) {
         try{
             //CODE 201: Se crea el mensaje y lo devuelve
             Mensaje mensaje  = servicio.postMensaje(Mapper.toMensaje(mensajeNuevoDTO));
             URI uri = builder
-                    .path(String.format("/%d", mensaje.getIdMensaje()))
+                    .path(String.format("/%d", centro.getIdCentro(), "/%d", mensaje.getIdMensaje()))
                     .build()
                     .toUri();
             return ResponseEntity.created(uri).body(Mapper.toMensajeDTO(mensaje));
